5fe479aa0409ef015c2cc64f9dd43e63e2747b73
Asher Cohen <asher.cohen@amplitude.com>
2026-07-05 20:00:56 -0700

    Revert error:unknown -> error:Error for QueryCache.onError compat

--- packages/web/src/lib/query-client.ts
+++ packages/web/src/lib/query-client.ts
@@ -2,8 +2,8 @@ import { QUERY_CACHE_MAX_AGE_MS } from "@dofek/scoring/query-cache";
 import { type Query, QueryCache, QueryClient } from "@tanstack/react-query";
 import { captureException } from "./telemetry.ts";
 
-function reportQueryError(error: unknown, query: Query) {
-  captureException(error instanceof Error ? error : new Error(String(error)), {
+function reportQueryError(error: Error, query: Query) {
+  captureException(error, {
     source: "react-query",
     queryHash: query.queryHash,
     failureCount: query.state.fetchFailureCount,
