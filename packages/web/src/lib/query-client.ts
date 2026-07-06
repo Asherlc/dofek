d245c11e1785cdb867f22f301b759361fb18a2d3
Asher Cohen <asher.cohen@amplitude.com>
2026-07-05 20:05:34 -0700

    Inline query error handler to fix TS contravariance

--- packages/web/src/lib/query-client.ts
+++ packages/web/src/lib/query-client.ts
@@ -1,19 +1,17 @@
 import { QUERY_CACHE_MAX_AGE_MS } from "@dofek/scoring/query-cache";
-import { type Query, QueryCache, QueryClient } from "@tanstack/react-query";
+import { QueryCache, QueryClient } from "@tanstack/react-query";
 import { captureException } from "./telemetry.ts";
-
-function reportQueryError(error: Error, query: Query) {
-  captureException(error, {
-    source: "react-query",
-    queryHash: query.queryHash,
-    failureCount: query.state.fetchFailureCount,
-  });
-}
 
 export function createAppQueryClient() {
   return new QueryClient({
     queryCache: new QueryCache({
-      onError: reportQueryError,
+      onError(error, query) {
+        captureException(error, {
+          source: "react-query",
+          queryHash: query.queryHash,
+          failureCount: query.state.fetchFailureCount,
+        });
+      },
     }),
     defaultOptions: {
       queries: {
