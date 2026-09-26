1f8f3b858313070327d86a48b97da6f30fea6187
Asher Cohen <asherlc@asherlc.com>
2026-09-25 20:34:25 -0700

    fix(ci): satisfy SQLFluff indentation in 0126 migration

--- drizzle/0126_mcp_oidc_adapter_user_ownership.sql
+++ drizzle/0126_mcp_oidc_adapter_user_ownership.sql
@@ -3,7 +3,8 @@ ADD COLUMN user_id uuid REFERENCES fitness.user_profile (id) ON DELETE CASCADE;
 --> statement-breakpoint
 UPDATE fitness.mcp_oidc_adapter
 SET user_id = uid::uuid
-WHERE uid ~ '^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$'
+WHERE
+  uid ~ '^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$'
   AND user_id IS NULL;
 --> statement-breakpoint
 CREATE INDEX mcp_oidc_adapter_user_idx
